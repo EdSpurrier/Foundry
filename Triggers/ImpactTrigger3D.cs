9be88450d1c6e21202d360cce3584b187ed388af
EdSpurrier <ed@edspurrier.com>
2026-05-06 10:26:09 +1000

    Foundry:  - Updated ImpactTrigger3D - inspector view

--- Triggers/ImpactTrigger3D.cs
+++ Triggers/ImpactTrigger3D.cs
@@ -9,14 +9,16 @@ namespace Foundry.Triggers
     [RequireComponent(typeof(Collider))]
     public class ImpactTrigger3D : MonoBehaviour
     {
+        [BoxGroup("Events")]
+        [FoldoutGroup("Events/On Impact")]
+        [HideLabel]
+        [SerializeField] protected FrameCoreEvent onImpact;
+        
         [Title("Settings")]
         [SerializeField] protected bool active = true;
 
         [Title("Filter")]
         [SerializeField] protected LayerMask detectionMask = ~0;
-
-        [Title("Events")]
-        [SerializeField] protected FrameCoreEvent onImpact;
 
         [Title("System")]
         [ReadOnly]
